405584918264815dce644238bcb997b9584127bb
Nagesh2003 <codeinovator@gmail.com>
2024-12-25 13:06:40 +0530

    chnages

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://time-table-viewer.vercel.app'
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads")); // Serve uploaded files
 
